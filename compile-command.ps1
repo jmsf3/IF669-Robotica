4339dd7fe4a8a47b7af547a821fd5c5d09dee5ee
João Marcelo <128498401+jmsf3@users.noreply.github.com>
2023-04-08 13:20:34 -0300

    Add files via upload

---
+++ compile-command.ps1
@@ -0,0 +1,1 @@
+g++ .\src\main.cpp .\dep\include\remoteApi\extApi.c .\dep\include\remoteApi\extApiPlatform.c .\src\coppelia\coppelia.cpp .\src\leitura\leitura.cpp .\src\movimento\movimento.cpp -o projeto-coppelia -lwinmm -lws2_32
